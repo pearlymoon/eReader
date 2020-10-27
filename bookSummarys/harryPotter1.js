8778a646b5884cdd83c6cf68eeec04f82d36a889
xSpyroex <68779200+xSpyroex@users.noreply.github.com>
2020-10-27 17:23:00 -0500

    adding scripts

---
+++ bookSummarys/harryPotter1.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./harryPotter1script.txt', 'utf8');
+
+console.log(data);
---
+++ movieScripts/beeMovie.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const data = fs.readFileSync('./beeMoviescript.txt', 'utf8')
+console.log(data)
