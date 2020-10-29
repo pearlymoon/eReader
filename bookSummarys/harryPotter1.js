f1f4ec29cc2187ba025888a8e4d76267cedd9c34
xSpyroex <68779200+xSpyroex@users.noreply.github.com>
2020-10-28 22:05:15 -0500

    Adding harrypotter

--- bookSummarys/harryPotter1.js
+++ bookSummarys/harryPotter1.js
@@ -3,3 +3,5 @@ const fs = require('fs');
 const data = fs.readFileSync('./harryPotter1script.txt', 'utf8');
 
 console.log(data);
+console.log('--------------')
+console.log('Word Count: '+data.length)
